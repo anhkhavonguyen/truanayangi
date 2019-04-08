6a2d197060409101dd632dcd82891665e6591582
ORIENTSOFTWARE\vnakha <vonguyen.anhkha@gmail.com>
2019-04-08 16:51:31 +0700

    change config

--- food-service.js
+++ food-service.js
@@ -4,7 +4,6 @@ var router = require('./routes/api-routes');
 const app = express();
 const mongoose = require("mongoose");
 
-// setup the Express middlware
 require('./middlewares/middleware')(app);
 
 app.use((req, res, next) => {
@@ -15,8 +14,6 @@ app.use((req, res, next) => {
     }
   });
   
-
-// setup the routes
 app.use("/", router);
 
 app.listen(config.port, () => {
